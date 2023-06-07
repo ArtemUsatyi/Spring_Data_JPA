6721bbe17d593d9537e969328622d5f3fa651352
artemowork <artem180791@mail.ru>
2023-06-07 23:09:19 +0300

    Файлы для записи/чтения к БД.

---
+++ src/main/java/ru/spring/dataJPA/services/PeopleServices.java
@@ -0,0 +1,44 @@
+package ru.spring.dataJPA.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.spring.dataJPA.models.Person;
+import ru.spring.dataJPA.repositories.PeopleRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class PeopleServices {
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleServices(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAllPeople() {
+        return peopleRepository.findAll();
+    }
+
+    public Person findOne(int id) {
+        return peopleRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        peopleRepository.save(person);
+    }
+
+    @Transactional
+    public void update(Person updatePerson, int id) {
+        updatePerson.setId(id);
+        peopleRepository.save(updatePerson);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        peopleRepository.deleteById(id);
+    }
+}
